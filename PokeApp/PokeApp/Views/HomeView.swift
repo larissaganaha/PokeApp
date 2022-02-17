0a5df35f4ad7cf3fd108e98bb641921bc8b68490
Larissa Ganaha <larissaganaha@gmail.com>
2022-02-17 20:45:58 -0300

    Adding Stacks to represent pokemon types

--- PokeApp/PokeApp/Views/HomeView.swift
+++ PokeApp/PokeApp/Views/HomeView.swift
@@ -16,7 +16,47 @@ struct HomeView: View {
                 endPoint: .bottom
             )
                 .ignoresSafeArea()
+            VStack(spacing: 20) {
+                Text("PokéApp")
+                    .font(.system(size: 40, weight: .bold, design: .monospaced))
+                    .foregroundColor(.white)
+                    .padding()
 
+                Spacer()
+
+                HStack {
+                    TypesView(type: "Normal", iconName: "circle.dashed")
+                    TypesView(type: "Fight", iconName: "circle.dashed")
+                    TypesView(type: "Flying", iconName: "circle.dashed")
+                }
+                HStack {
+                    TypesView(type: "Poison", iconName: "circle.dashed")
+                    TypesView(type: "Ground", iconName: "circle.dashed")
+                    TypesView(type: "Rock", iconName: "circle.dashed")
+                }
+                HStack {
+                    TypesView(type: "Bug", iconName: "circle.dashed")
+                    TypesView(type: "Ghost", iconName: "circle.dashed")
+                    TypesView(type: "Steel", iconName: "circle.dashed")
+                }
+                HStack {
+                    TypesView(type: "Fire", iconName: "circle.dashed")
+                    TypesView(type: "Water", iconName: "circle.dashed")
+                    TypesView(type: "Grass", iconName: "circle.dashed")
+                }
+                HStack {
+                    TypesView(type: "Electric", iconName: "circle.dashed")
+                    TypesView(type: "Psychic", iconName: "circle.dashed")
+                    TypesView(type: "Ice", iconName: "circle.dashed")
+                }
+                HStack {
+                    TypesView(type: "Dragon", iconName: "circle.dashed")
+                    TypesView(type: "Dark", iconName: "circle.dashed")
+                    TypesView(type: "Fairy", iconName: "circle.dashed")
+                }
+
+                Spacer()
+            }
         }
     }
 }
@@ -29,4 +69,26 @@ struct HomeView_Previews: PreviewProvider {
 
 extension Color {
     static let gradientBottom = Color("BottomGradientColor")
+}
+
+struct TypesView: View {
+    var type: String
+    var iconName: String
+
+    var body: some View {
+        VStack(spacing: -5) {
+            Image(systemName: iconName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+                .colorInvert()
+            Text(type)
+                .font(.system(size: 20, weight: .regular, design: .monospaced))
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity)
+                .padding()
+
+        }
+    }
 }
